2c6f9a748a2de25870fe20452cfbaa87d650e6bb
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-10-24 13:11:34 +0900

    :seedling: Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("");
+    if name == "" {
+        format!("Keep greeting world forever!")
+    } else {
+        format!("Hello {}!", &name)
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
